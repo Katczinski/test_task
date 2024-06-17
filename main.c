f3fcfc98a3c2d354bf349c1ad33d595f81c9e020
Katczinski <greench41@ya.ru>
2024-06-17 22:52:56 +0300

    testing_indent

--- main.c
+++ main.c
@@ -4,34 +4,34 @@
 #include "utils.h"
 
 enum {
-    RET_OK,
-    RET_ERROR,
+	RET_OK,
+	RET_ERROR,
 };
 
 int validate_argv(int argc, char *argv[])
 {
-    (void)argv;
-    if (argc != 5) {
-        log_add("Incorrect number of arguments");
+	(void)argv;
+	if (argc != 5) {
+		log_add("Incorrect number of arguments");
 		return RET_ERROR;
-    }
-	
+	}
+
 	if (!is_valid_ip(argv[1])) {
 		log_add("Invalid UDP IP and/or port");
 		return RET_ERROR;
 	}
-	
+
 	return RET_OK;
 }
 
 int main(int argc, char *argv[])
 {
-    int ret = validate_argv(argc, argv); 
+	int ret = validate_argv(argc, argv); 
 
-    if (ret == RET_OK)
-    {
+	if (ret == RET_OK)
+	{
 
-    }
+	}
 
-    return ret;
+	return ret;
 }
